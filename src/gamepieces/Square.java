481ce4dca1285afe3539142ad7f3480639208433
Craig <craigworthington33@gmail.com>
2019-10-20 16:29:32 -0400

    Added JavaDoc comments to square file

--- src/gamepieces/Square.java
+++ src/gamepieces/Square.java
@@ -8,6 +8,11 @@ public class Square {
 	private int row;
 	private String name;
 	
+	/**
+	 * Constructor for Square object
+	 * @param x The x coordinate of the square
+	 * @param y The y coordinate of the square
+	 */
 	public Square(int x, int y) {
 		//board is set to 5x5
 		if(x>4 || y>4) throw new IllegalArgumentException("unavailable position");
@@ -17,23 +22,45 @@ public class Square {
 		name = null;
 	}
 	
+	/**
+	 * Constructor for square object, with name
+	 * @param x The x coordinate of this square
+	 * @param y The y coordinate of this square
+	 * @param name The name of the object in this square
+	 */
 	public Square(int x, int y, String name) {
 		this(x,y);
 		this.name = name;
 	}
 	
+	/**
+	 * Get the row of this square
+	 * @return the x location of this square
+	 */
 	public int getRow() {
 		return row;
 	}
 	
+	/**
+	 * Get the column of the location of this square
+	 * @return The y location of this square
+	 */
 	public int getColumn() {
 		return column;
 	}
 	
+	/**
+	 * Set the name of the object in the square
+	 * @param name The name of the object for this square
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	/**
+	 * Get the name of the object in the square
+	 * @return the name of the object in the square
+	 */
 	public String getName() {
 		return name;
 	}
@@ -45,16 +72,29 @@ public class Square {
 		return false;
 	}
 	
+	/**
+	 * Move the piece to a new location
+	 * @param x Horizontal coordinate of the new location
+	 * @param y Vertical location of the new coordinate
+	 */
 	public void Move(int x, int y) {
+		// TODO: add protections here for values outside of ranges of valid squares
 		row = x;
 		column = y;
 	}
 	
+	/**
+	 * Test if the square is occupied
+	 * @return true if the space is occupied
+	 */
 	public boolean isOccupied() {
 		//tell if the square is occupied
 		return (name != null && (!name.equals("  Hole ")));
 	}
 	
+	/**
+	 * toString function for this square
+	 */
 	public String toString() {
 		if(name == null) {
 			return " (" + row + "," + column + ") ";
