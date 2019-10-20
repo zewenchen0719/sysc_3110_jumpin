213d0b3445e60526e4ebd94d289d3fc08c49dfea
Craig <craigworthington33@gmail.com>
2019-10-20 17:15:31 -0400

    Added JavaDoc comments to CommandWord

--- src/CommandWord.java
+++ src/CommandWord.java
@@ -18,20 +18,37 @@ public class CommandWord {
 		
 	}
 	
+	/**
+	 * Test if the string is a valid command
+	 * @param str The string to test
+	 * @return True if the string is a valid command
+	 */
 	public boolean isCommand(String str) {
-		for(int i=0; i<validCommands.length; i++) {
-			if(validCommands[i].equals(str))return true;
+		for (int i=0; i<validCommands.length; i++) {
+			if (validCommands[i].equals(str)) {
+				return true;
+			}
 		}
 		return false;
 	}
 	
+	/**
+	 * Test if the string is a valid name
+	 * @param str the string to test
+	 * @return True if the string is a valid name
+	 */
 	public boolean isName(String str) {
-		for(int i=0; i<validNames.length; i++) {
-			if(validNames[i].equals(str))return true;
+		for (int i=0; i<validNames.length; i++) {
+			if (validNames[i].equals(str)) {
+				return true;
+			}
 		}
 		return false;
 	}
 	
+	/**
+	 * Show all commands on the console
+	 */
 	public void showAll() {
 		for(String command: validCommands) {
 			System.out.print(command + " ");
