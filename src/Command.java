d3dda9d5e867f1810df7e90dffce45bef987a167
Craig <craigworthington33@gmail.com>
2019-10-20 17:30:23 -0400

    Added JavaDoc to command class

--- src/Command.java
+++ src/Command.java
@@ -8,6 +8,13 @@ public class Command {
 	
 	private int num;
 	
+	/**
+	 * Constructor for the Command class
+	 * @param firstWord The first command word
+	 * @param secondWord The second command word
+	 * @param thirdWord The third command word
+	 * @param num the number to be added
+	 */
 	public Command(String firstWord, String secondWord, String thirdWord, int num) {
 		commandWord = firstWord;
 		name = secondWord;
@@ -15,6 +22,12 @@ public class Command {
 		this.num = num;
 	}
 	
+	/**
+	 * Constructor for the Command class
+	 * @param firstWord The first command word
+	 * @param secondWord The second command word
+	 * @param thirdWord The third command word
+	 */
 	public Command(String firstWord, String secondWord, String thirdWord) {
 		commandWord = firstWord;
 		name = secondWord;
@@ -22,14 +35,26 @@ public class Command {
 		this.num = -1;
 	}
 	
+	/**
+	 * Get the name in the command
+	 * @return the name in the command
+	 */
 	public String getNameCalled() {
 		return name;
 	}
 	
+	/**
+	 * Get the command word from the command
+	 * @return the command word
+	 */
 	public String getCommandWord() {
 		return commandWord;
 	}
 	
+	/**
+	 * Get the second word in the command
+	 * @return the second word
+	 */
 	public String getSecondWord() {
 		return secondWord;
 	}
@@ -38,20 +63,36 @@ public class Command {
 		return num;
 	}
 	
+	/**
+	 * Test if the command word is unknown
+	 * @return True if commandWord is null
+	 */
 	public boolean isUnknown() {
-		return commandWord==null;
+		return commandWord == null;
 	}
 	
+	/**
+	 * Test if the second command exists
+	 * @return True if secondWord is not null
+	 */
 	public boolean hasSecondCommand() {
-		return secondWord!=null;
+		return secondWord != null;
 	}
 	
+	/**
+	 * Test if the command has a number
+	 * @return True if num is not -1
+	 */
 	public boolean hasNum() {
 		return num != -1;
 	}
 	
+	/**
+	 * Test if the command has a name
+	 * @return True if name is not null
+	 */
 	public boolean hasName() {
-		return name!= null;
+		return name != null;
 	}
 
 }
